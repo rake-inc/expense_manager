e037c7fc7691337e21564400b47c5a9d5430a4ca
gautham <gautham.nambi@live.com>
2017-09-17 00:34:53 +0530

    added gitignore

--- rest_pro/rest_pro/settings.py
+++ rest_pro/rest_pro/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'rest_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev_test',
+        'USER': 'postgres',
+        'PASSWORD': 'crunchbang',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
